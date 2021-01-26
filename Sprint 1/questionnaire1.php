9b8333cd13abd94c2fd580e2263c354df410e45c
BruceAlanMiller <55394386+BruceAlanMiller@users.noreply.github.com>
2021-01-26 11:08:04 +0000

    Update questionnaire1.php

--- Sprint 1/questionnaire1.php
+++ Sprint 1/questionnaire1.php
@@ -11,14 +11,14 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <title>formDemo.html</title>
-<link href="petstyle.css" type="text/css" rel="stylesheet" />
+
 
 </head>
 <body>
 <table>
 <tr>
 <td width="650" height="100"> 
-<img src="pshopimage.png" width="150" height="150">
+<img src="" width="150" height="150">
 </td>
 
 <td width="700" height="100">
