ccbc3f023a149506484c56a405d323f631c5d46f
Jordan <j.l.keiller@dundee.ac.uk>
2021-01-26 15:32:00 +0000

    Added the MYSQL Connection Details

---
+++ Sprint 1/Website/php/__GLOBAL_CONFIG__.php
@@ -0,0 +1,19 @@
+<?php
+    // MYSQL Database Connection Details
+    $DATABASE_HOST = "silva.computing.dundee.ac.uk"; 
+    $DATABASE_NAME = "20agileteam2db";
+    $DATABASE_USERNAME = "20agileteam2";
+    $DATABASE_PASSWORD = "7343.at2.3437";
+
+    try {
+        // Attempts the MYSQL Connection
+        $MYSQL_CONNECTION = new PDO("mysql:host=".$DATABASE_HOST.";dbname=".$DATABASE_NAME, $DATABASE_USERNAME, $DATABASE_PASSWORD);
+
+        // Sets the PDO error mode to exception
+        $MYSQL_CONNECTION->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      } catch(PDOException $e) {
+
+        // Error message if MYSQL Connection failed
+        echo "Error:" . $e->getMessage();
+      }
+?>
