4308101058b42ccff977522021f874c7c588293d
Jordan <j.l.keiller@dundee.ac.uk>
2021-01-27 11:56:54 +0000

    Takes input from form and processes

--- Sprint 1/Website/php/questionnaire_processing.php
+++ Sprint 1/Website/php/questionnaire_processing.php
@@ -1,7 +1,33 @@
 <?php
-var_dump($_POST);
 
-foreach ($_POST as $key => $value){
-    echo $value;
+include "__GLOBAL_CONFIG__.PHP";
+
+if (isset($_POST['submit'])){
+
+    foreach($_POST as $key => $value){
+        if($key != "submit"){
+            $SQL_QUERY_QUESTIONS_OPTIONS = "CALL 20agileteam2db.get_question_option(".$key.", '".$value."');";
+            $STMT_OPTIONS = $MYSQL_CONNECTION->prepare($SQL_QUERY_QUESTIONS_OPTIONS);
+            $STMT_OPTIONS->execute();
+            $RESULT_OPTIONS = $STMT_OPTIONS->fetchAll();
+        
+            foreach($RESULT_OPTIONS as $row){
+                echo $key."<br>".$row['Question_Option_ID']."<br>".$value."<br><br>";
+            }
+        }
+        else{
+            echo "";
+        }
+        
+        // echo "<li>$key: $value</li>";
+    
+    
+    }
+
+
+
 }
+
+
+
 ?>
