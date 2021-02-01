9c5e1b8853629dab40cdbd9da0d607e27ae6c51e
Jordan <j.l.keiller@dundee.ac.uk>
2021-02-01 14:07:34 +0000

    Quiz title read through PHP & dynamic

--- Software_Code/_site/assets/php/questionnaire_title.php
+++ Software_Code/_site/assets/php/questionnaire_title.php
@@ -1,9 +1,7 @@
 <?php
-
-    $SQL_QUERY_QUESTIONS = "SELECT * FROM `questionnaire_questions` WHERE `Questionnaire_ID` = ".$_GET['quiz_id']."";
+    $SQL_QUERY_QUESTIONS = "SELECT * FROM `questionnaire` WHERE `Questionnaire_ID` = ".$_GET['quiz_id']."";
     $STMT = $MYSQL_CONNECTION->prepare($SQL_QUERY_QUESTIONS);
     $STMT->execute();
-    $RESULT = $STMT->fetchAll();
-
-
+    $RESULT = $STMT->fetch();
+    echo "<h1 class='text-primary fw-bold mt-3 mb-0'>".$RESULT['Questionnaire_Name']."</h1>";
 ?>
