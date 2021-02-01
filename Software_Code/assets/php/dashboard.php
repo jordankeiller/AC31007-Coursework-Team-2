289629a302daa47cceffb0cb938e784b787cf9fc
MikolajOlejnik <molejnik@dundee.ac.uk>
2021-02-01 20:39:25 +0000

    Questionnaires
    Fetch researcher questionnaires

--- Software_Code/assets/php/dashboard.php
+++ Software_Code/assets/php/dashboard.php
@@ -1,6 +1,7 @@
 <?php
 include "GLOBAL_CONFIG.php";
 
+  //If not logged in
   if(!isset($_SESSION['researcherType'])) {
     header("location: ../../login.php");
   }
@@ -21,8 +22,19 @@ include "GLOBAL_CONFIG.php";
     <title>Dashboard</title>
   </head>
   <body>
-    <?php if($_SESSION['researcherType'] == 'PR'){
-      echo "test";
+
+    <?php if($_SESSION['researcherType'] == 'PR'){ // Shown to Principal Researchers
+      $FETCH_RESEARCHER_QUESTIONNAIRES = "CALL `20agileteam2db`.`researcher_questionnaires`(".$_SESSION['userid'].")";
+      $STMT = $MYSQL_CONNECTION->prepare($FETCH_RESEARCHER_QUESTIONNAIRES);
+      $STMT->execute();
+      $RESEARCHER_QUESTIONNAIRES = $STMT->fetchall();
+      echo count($RESEARCHER_QUESTIONNAIRES);
+      echo "<h1>hello principal</h1>";
+
+        foreach ($RESEARCHER_QUESTIONNAIRES as $ROW) {
+          echo "<button type='button'>" . $ROW['Questionnaire Name'] . "</button>";
+        }
+
     } ?>
   </body>
 </html>
