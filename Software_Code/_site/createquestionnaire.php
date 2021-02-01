6ccc2170554dba45a3c217a79f88f965eb05dd81
Jordan <j.l.keiller@dundee.ac.uk>
2021-02-01 19:13:58 +0000

    Changed variable names

--- Software_Code/_site/createquestionnaire.php
+++ Software_Code/_site/createquestionnaire.php
@@ -33,66 +33,78 @@
     </div>
   </nav>
   <div class="container bg-white px-4">
+    <div class="row">
+      <div class="col">
+        <h1 class="text-primary fw-bold mt-3 mb-0">Create Questionnaire</h1>
+      </div>
       <div class="row">
         <div class="col">
-            <h1 class="text-primary fw-bold mt-3 mb-0">Create Questionnaire</h1>
-        </div>
-        <div class="row">
-            <div class="col">
-                
-      
+
+
 
           <label for="createQuiz">Choose a Question Type:</label>
           <select name="question_type" id="question_type" form="createQuiz" onchange="dropdownTypeChanged()"><?php include "assets/php/show_question_type.php" ?></select>
 
-          
-
-          <div class="card mt-3" id="type_text" style="display: none;">
-            <div class="card-body">
-              <div class="form-floating">
-                <textarea class="form-control" placeholder="text here" id="type_text_field" style="height: 100px"></textarea>
-                <label for="option_text_field">Question Name</label>
-              </div>
-            </div>
-          </div>
-
-          <div class="card mt-3" id="type_number" style="display: none;">
-            <div class="card-body">
-              <div class="mb-3">
-                <div class="form-floating">
-                  <input type="number" class="form-control" id="type_number_field" placeholder="Password">
-                  <label for="option_number_field">Number</label>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="card mt-3" id="type_multi_select" style="display: none;">
-            <div class="card-body">
-              <div class="mb-3">
-                <div class="input-group mb-3">
-                  <div class="input-group-text">
-                    <input class="form-check-input" type="checkbox">
-                  </div>
-                  <input type="text" class="form-control">
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="card mt-3" id="type_option" style="display: none;">
-            <div class="card-body">
-              <div class="mb-3">
-                <div class="input-group">
-                  <div class="input-group-text">
-                    <input class="form-check-input" type="radio" value="">
-                  </div>
-                  <input type="text" class="form-control">
-                </div>
-              </div>
-            </div>
-          </div>
           <form id="createQuiz" action="assets/php/check_question_type.php" method="post">
+
+            <div class="card mt-3" id="type_text" style="display: none;">
+              <div class="card-body">
+                <div class="form-floating">
+                  <input name="1" class="form-control" placeholder="text here" id="type_text_field" style="height: 100px">
+                  <label for="option_text_field">Question Name</label>
+                </div>
+              </div>
+            </div>
+
+            <div class="card mt-3" id="type_number" style="display: none;">
+              <div class="card-body">
+                <div class="mb-3">
+                  <div class="form-floating">
+                    <input name="2" type="number" class="form-control" id="type_number_field" placeholder="Password">
+                    <label for="option_number_field">Number</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="card mt-3" id="type_multi_select" style="display: none;">
+              <div class="card-body">
+                <div class="mb-3">
+                  <div class="input-group mb-3">
+                    <div class="input-group-text">
+                      <input name="3" class="form-check-input" type="checkbox">
+                    </div>
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="card mt-3" id="type_option" style="display: none;">
+              <div class="card-body">
+                <div class="mb-3">
+                  <div class="input-group">
+                    <div class="input-group-text">
+                      <input name="4" class="form-check-input" type="radio" value="">
+                    </div>
+                    <input type="text" class="form-control">
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
             <input type="submit" name="submit" id="submit">
           </form>
         </div>
