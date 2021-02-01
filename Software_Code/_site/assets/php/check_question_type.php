4ba825ea5db18e9b92d331163d0df7d99660079a
Jordan <j.l.keiller@dundee.ac.uk>
2021-02-01 12:55:40 +0000

    Testing question types

--- Software_Code/_site/assets/php/check_question_type.php
+++ Software_Code/_site/assets/php/check_question_type.php
@@ -1,7 +1,18 @@
 <?php
+    include "GLOBAL_CONFIG.php";
 
     if(isset($_POST['question_type'])){
-        echo $_POST['question_type'];
+        echo "<strong>SUBMITTED RESPONSE:</strong><br>" . $_POST['question_type'];
+    }
+
+    $FETCH_QUESTION_TYPE = "SELECT * FROM 20agileteam2db.question_types";
+    $STMT = $MYSQL_CONNECTION->prepare($FETCH_QUESTION_TYPE);
+    $STMT->execute();
+    $RESULT = $STMT->fetchAll();
+
+    echo "<br>";
+    foreach($RESULT as $row){
+        echo "<br>".$row['Name'];
     }
 
 
