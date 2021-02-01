aaf58c9f052510839038176a7dc8a2f699dd2a2b
FreetimeFM <awesomea380@tutanota.com>
2021-02-01 15:36:44 +0000

    Updated function to force display element

--- Software_Code/_site/assets/js/createQuestionnaire.js
+++ Software_Code/_site/assets/js/createQuestionnaire.js
@@ -1,6 +1,5 @@
-function showHideElement(id) {
-  
-  if (document.getElementById(id).style.display == "none") {
+function showHideElement(id, bool) {
+  if (bool) {
     document.getElementById(id).style.display = "block";
   } else {
     document.getElementById(id).style.display = "none";
