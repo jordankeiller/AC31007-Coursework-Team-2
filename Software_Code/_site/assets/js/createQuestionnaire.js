99909285f13185282ee3b747580080c5c0f940f2
FreetimeFM <awesomea380@tutanota.com>
2021-02-01 15:38:39 +0000

    Added function which shows elements based on dropdown selection

--- Software_Code/_site/assets/js/createQuestionnaire.js
+++ Software_Code/_site/assets/js/createQuestionnaire.js
@@ -5,3 +5,35 @@ function toggleElement(id, bool) {
     document.getElementById(id).style.display = "none";
   }
 }
+
+function dropdownTypeChanged() {
+
+  if (document.getElementById("question_type").value == "Text") {
+    toggleElement('type_text', true);
+    toggleElement('type_number', false);
+    toggleElement('type_multi_select', false);
+    toggleElement('type_option', false);
+  }
+  else if (document.getElementById("question_type").value == "Whole Number") {
+    toggleElement('type_text', false);
+    toggleElement('type_number', true);
+    toggleElement('type_multi_select', false);
+    toggleElement('type_option', false);
+  }
+  else if (document.getElementById("question_type").value == "Tick all that apply") {
+    toggleElement('type_text', false);
+    toggleElement('type_number', false);
+    toggleElement('type_multi_select', true);
+    toggleElement('type_option', false);
+  }
+  else if (document.getElementById("question_type").value == "Pick one option") {
+    toggleElement('type_text', false);
+    toggleElement('type_number', false);
+    toggleElement('type_multi_select', false);
+    toggleElement('type_option', true);
+  }
+  else {
+    alert("Error!");
+  }
+
+}
