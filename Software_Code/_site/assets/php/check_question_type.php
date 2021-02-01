76961b290cdd1d090f4e4ad5e02684590e5c5d54
Jordan <j.l.keiller@dundee.ac.uk>
2021-02-01 19:14:14 +0000

    Hardcode insert text to DB

--- Software_Code/_site/assets/php/check_question_type.php
+++ Software_Code/_site/assets/php/check_question_type.php
@@ -1,9 +1,19 @@
 <?php
     include "GLOBAL_CONFIG.php";
 
-    if(isset($_POST['response1'])){
-        echo "<strong>SUBMITTED RESPONSE:</strong><br>" . $_POST['response1'];
+    foreach($_POST as $key => $value){
+        
+        // echo $key . ": ". $value ."<br>";
+        if($key == 1){
+            $insertText = "CALL `20agileteam2db`.`create_question`(2, '".$value."', 1)";
+            $STMT_ENTRY = $MYSQL_CONNECTION->prepare($insertText);
+            $STMT_ENTRY->execute();
+        }
     }
+
+    // if(isset($_POST['question_type'])){
+    //     echo "<strong>SUBMITTED RESPONSE:</strong><br>" . $_POST['question_type'];
+    // }
 
     $FETCH_QUESTION_TYPE = "SELECT * FROM 20agileteam2db.question_types";
     $STMT = $MYSQL_CONNECTION->prepare($FETCH_QUESTION_TYPE);
