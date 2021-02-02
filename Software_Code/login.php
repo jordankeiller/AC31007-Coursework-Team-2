5d7fc51ac02e5ad13191c9f5ddfd32d5d0fc6529
MikolajOlejnik <molejnik@dundee.ac.uk>
2021-02-02 15:18:25 +0000

    Update login.php
    Comments

--- Software_Code/login.php
+++ Software_Code/login.php
@@ -6,12 +6,15 @@ title: Login
     <div class="row">
         <div class="col align-self-center">
             <h2 class="text-primary mb-4">Log Into Your Questionnaire Extraordinare Account</h2>
+            <!-- Posts results to process.php -->
             <form action="assets/php/process.php" method="POST">
                 <div class="mb-4">
                     <label class="form-label" for="usernameInput">Username</label>
+                    <!-- Submit username to validate -->
                     <input type="text" name="username" class="form-control" id="usernameInput">
                 </div>
                 <div class="mb-4">
+                  <!-- Submit password to validate -->
                     <label class="form-label" for="passwordInput">Password</label>
                     <input type="password" name="userPass" class="form-control" id="passwordInput">
                 </div>
