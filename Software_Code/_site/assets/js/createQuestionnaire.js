52e3be6a16d0de60196672ec4d879cd1cd86c335
FreetimeFM <awesomea380@tutanota.com>
2021-02-02 19:17:38 +0000

    Create previews for text and number input

--- Software_Code/_site/assets/js/createQuestionnaire.js
+++ Software_Code/_site/assets/js/createQuestionnaire.js
@@ -28,6 +28,16 @@ function createQuestion() {
     alert('Please enter a question.')
     return;
   }
+  if (dropdownValue == "Text") {
+
+    // Creates attributes relevant to text input and displays in preview.
+    response.innerHTML = '<textarea class="form-control"></textarea>';
+
+  }
+  else if (dropdownValue == "Whole Number") {
+
+    response.innerHTML = '<input type="number" class="form-control" value="0">';
+
   }
   document.getElementById('question_input').value = '';
   document.getElementById('options_input').value = '';
