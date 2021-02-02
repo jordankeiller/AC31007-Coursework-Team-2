0645d0172bd00d24bbc5418c6c85682384fba24a
FreetimeFM <awesomea380@tutanota.com>
2021-02-02 19:14:54 +0000

    Simplified dropdownTypeChanged() function

--- Software_Code/_site/assets/js/createQuestionnaire.js
+++ Software_Code/_site/assets/js/createQuestionnaire.js
@@ -10,29 +10,15 @@ function toggleElement(id, bool) {
 // Shows/hides options based on dropdown selection.
 function dropdownTypeChanged() {
 
-  if (document.getElementById("question_type").value == "Text") {
-    toggleElement('type_text', true);
-    toggleElement('type_number', false);
-    toggleElement('type_multi_select', false);
-    toggleElement('type_option', false);
-  }
-  else if (document.getElementById("question_type").value == "Whole Number") {
-    toggleElement('type_text', false);
-    toggleElement('type_number', true);
-    toggleElement('type_multi_select', false);
-    toggleElement('type_option', false);
-  }
-  else if (document.getElementById("question_type").value == "Tick all that apply") {
-    toggleElement('type_text', false);
-    toggleElement('type_number', false);
-    toggleElement('type_multi_select', true);
-    toggleElement('type_option', false);
+  if (document.getElementById("question_type").value == "Tick all that apply") {
+    toggleElement('type_options', true);
   }
   else if (document.getElementById("question_type").value == "Pick one option") {
-    toggleElement('type_text', false);
-    toggleElement('type_number', false);
-    toggleElement('type_multi_select', false);
-    toggleElement('type_option', true);
+    toggleElement('type_options', true);
+  }
+  else {
+    toggleElement('type_options', false);
+  }
   }
 }
 
