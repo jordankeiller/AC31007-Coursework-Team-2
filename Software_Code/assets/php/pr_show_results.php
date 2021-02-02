505bd0da471f86e5f51a892a837d1dbfcfbc76cc
MikolajOlejnik <molejnik@dundee.ac.uk>
2021-02-02 15:26:21 +0000

    Update pr_show_results.php
    Commenting and minor input validation to check whether logged in as PR.

--- Software_Code/assets/php/pr_show_results.php
+++ Software_Code/assets/php/pr_show_results.php
@@ -2,27 +2,30 @@
 
 include "GLOBAL_CONFIG.php";
 
-if(!isset($_SESSION['researcherType'])) {
+// If not logged in (as Principal Researcher)
+if(!isset($_SESSION['researcherType']) || $_SESSION['researcherType']) != 'PR') {
   header("Location: ../../login.php");
   exit;
 }
 
+// Logged in as PR
 else {
 
-  $CURR_QUESTION = NULL;
+  $CURR_QUESTION = NULL; // Used for printing question names
   foreach ($_POST as $key => $value) {
     $FETCH_QUESTIONNAIRE_RESPONSES = "CALL `20agileteam2db`.`get_questions`(". $value .")";
     $STMT = $MYSQL_CONNECTION->prepare($FETCH_QUESTIONNAIRE_RESPONSES);
     $STMT->execute();
     $RESEARCHER_QUESTIONNAIRES = $STMT->fetchall();
 
+    // For each answer in questionnaire
     foreach ($RESEARCHER_QUESTIONNAIRES as $row) {
-      if ($row['Response'] != NULL) {
+      if ($row['Response'] != NULL) { // Open ended question
         echo $row['Description'] . "<br>";
         echo $row['Response']. "<br>";
       }
 
-      else {
+      else { // Select one of the following or tick all of the following
         if ($row["Question ID"] != $CURR_QUESTION) {
           $CURR_QUESTION = $row["Question ID"];
           echo $row['Description'] . "<br>";
@@ -32,6 +35,7 @@ else {
         $STMT->execute();
         $QUESTION_OPTIONS = $STMT->fetchall();
 
+        // Check the option corresponding to Option_ID
         foreach ($QUESTION_OPTIONS as $result) {
           if ($result['Option_ID'] == $row['Option ID']) {
             echo $result['Options'];
@@ -44,30 +48,4 @@ else {
   }
 
 }
-
-// foreach ($_POST as $key => $value) {
-//   $FETCH_QUESTIONNAIRE_RESPONSES = "CALL `20agileteam2db`.`get_questions`(". $value .")";
-//   $STMT = $MYSQL_CONNECTION->prepare($FETCH_QUESTIONNAIRE_RESPONSES);
-//   $STMT->execute();
-//   $RESEARCHER_QUESTIONNAIRES = $STMT->fetchall();
-//
-//   foreach ($RESEARCHER_QUESTIONNAIRES as $row) {
-//     if ($row['Response'] != NULL) {
-//       echo $row['Response']. "<br>";
-//     }
-//     else {
-//       $FETCH_QUESTION_OPTIONS = "CALL `20agileteam2db`.`options_for_question`(". $row['Question ID'] .")";
-//       $STMT = $MYSQL_CONNECTION->prepare($FETCH_QUESTION_OPTIONS);
-//       $STMT->execute();
-//       $QUESTION_OPTIONS = $STMT->fetchall();
-//
-//       foreach ($QUESTION_OPTIONS as $result) {
-//         if ($result['Option_ID'] == $row['Option ID']) {
-//           echo $result['Options']. "<br>";
-//         }
-//       }
-//     }
-//   }
-// }
-
 ?>
