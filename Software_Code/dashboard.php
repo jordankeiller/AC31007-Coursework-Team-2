19da69c7339abb3bb647f7a1115ecca3969ef8c1
Momchil Badzhev <mbadzhev@gmail.com>
2021-02-03 10:22:39 +0000

    dashboard now set up for jekyll use

--- Software_Code/dashboard.php
+++ Software_Code/dashboard.php
@@ -1,50 +1,47 @@
-<?php
-  include "assets/php/GLOBAL_CONFIG.php";
-
-  //If not logged in
-  if(!isset($_SESSION['researcherType'])) {
-    header("location: login.php");
-    exit;
-  }
-  // elseif($_SESSION['researcherType'] == ){
-  //
-  // }
-  //
-  // elseif($_SESSION['researcherType'] == ){
-  //
-  // }
-
-?>
-
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Dashboard</title>
-  </head>
-  <body>
-
-    <?php if($_SESSION['researcherType'] == 'PR'){ // Shown to Principal Researchers
+---
+layout: default
+title: Dashboard
+---
+<div class="container bg-white px-4">
+  <div class="row">
+    <div class="col">
+      <?php
+      include "assets/php/GLOBAL_CONFIG.php";
+      //If not logged in
+      if(!isset($_SESSION['researcherType'])) {
+        header("location: login.php");
+        exit;
+      }
+      // elseif($_SESSION['researcherType'] == ){
+      //
+      // }
+      //
+      // elseif($_SESSION['researcherType'] == ){
+      //
+      // }
+      ?>
+      <?php
+      if($_SESSION['researcherType'] == 'PR'){ // Shown to Principal Researchers
       // Show all of the given PR's questionnaires
       $FETCH_RESEARCHER_QUESTIONNAIRES = "CALL `20agileteam2db`.`researcher_questionnaires`(".$_SESSION['userid'].")";
       $STMT = $MYSQL_CONNECTION->prepare($FETCH_RESEARCHER_QUESTIONNAIRES);
       $STMT->execute();
-      $RESEARCHER_QUESTIONNAIRES = $STMT->fetchall(); } ?>
+      $RESEARCHER_QUESTIONNAIRES = $STMT->fetchall(); }
+      ?>
 
-      <p>Which questionnaire responses would you like to see?</p>
+      <h1 class="text-primary">Which questionnaire responses do you want to see?</h1>
 
       <!-- Dropdown to select questionnaire -->
       <form action="pr_show_results.php" method="POST">
         <label for="questionnaires">Questionnaire:</label>
         <select name="questionnaire" id="questionnaire">
-           <?php foreach ($RESEARCHER_QUESTIONNAIRES as $ROW) {
+          <?php foreach ($RESEARCHER_QUESTIONNAIRES as $ROW) {
             echo "<option name='". $ROW['Questionnaire ID'] . "' value='" . $ROW['Questionnaire ID'] . "'>" . $ROW['Questionnaire Name'] . "</option>";
           } ?>
         </select>
         <br><br>
-        <input type="submit" value="Submit">
+        <input id="submit" name="submit" class="btn btn-lg btn-primary mt-0 mb-4" type="submit" value="Submit">
       </form>
-
-
-  </body>
-</html>
+    </div>
+  </div>
+</div>
