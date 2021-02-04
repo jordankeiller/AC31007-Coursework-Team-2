d13d4f45b3dcfd3fc2dbd02b6bba6321f0762fb1
FreetimeFM <awesomea380@tutanota.com>
2021-02-04 17:45:41 +0000

    Added bootstrap scripts

--- Software_Code/_site/manage_login.php
+++ Software_Code/_site/manage_login.php
@@ -130,5 +130,9 @@
 
 	</div>
 	</form>
+
+	<script src="https://unpkg.com/@popperjs/core@2.4.0/dist/umd/popper.min.js"></script>
+	<script src="assets/js/bootstrap.js"></script>
 </body>
+
 </html>
