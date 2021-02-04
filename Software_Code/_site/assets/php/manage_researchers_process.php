eb6c1012b8008b819bafda4b6adb1bff1aec5c53
FreetimeFM <awesomea380@tutanota.com>
2021-02-04 14:43:26 +0000

    Updated action messages

--- Software_Code/_site/assets/php/manage_researchers_process.php
+++ Software_Code/_site/assets/php/manage_researchers_process.php
@@ -22,8 +22,8 @@
 		// Sends query with entered details to the database.
 		$MYSQL_CONNECTION->prepare("INSERT INTO researcher (Name, Researcher_Type) VALUES ('$name', '$Type')")->execute();
 
-		$_SESSION['message'] = "researcher saved"; 
 		header('location: index.php');
+		$_SESSION['message'] = "<strong>Success! </strong>Researcher saved."; 
 	}
 	// If the user wants to update researcher details.
 	else if (isset($_POST['update'])) {
@@ -36,8 +36,8 @@
 		// Sends query to update details
 		$MYSQL_CONNECTION->prepare("UPDATE researcher SET Name='$name', Researcher_Type='$Type' WHERE Researcher_ID=$id")->execute();
 
-		$_SESSION['message'] = "Address updated!"; 
 		header('location: index.php');
+		$_SESSION['message'] = "<strong>Success! </strong>Researcher updated.";
 	}
 	// If the user wants to delete a researcher.
 	else if (isset($_GET['del'])) {
@@ -48,7 +48,7 @@
 		// Execute delete statement.
 		$MYSQL_CONNECTION->prepare("DELETE FROM researcher WHERE Researcher_ID=$id")->execute();
 
-		$_SESSION['message'] = "Researcher deleted!"; 
 		header('location: index.php');
+		$_SESSION['message'] = "<strong>Success! </strong>Researcher deleted."; 
 	}
 ?>
