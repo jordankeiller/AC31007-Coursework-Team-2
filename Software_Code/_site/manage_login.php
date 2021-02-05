dc6fde9f83d1f36a08284c3f6c3267c90c91f7b0
FreetimeFM <awesomea380@tutanota.com>
2021-02-05 10:48:42 +0000

    changed logout button href link

--- Software_Code/_site/manage_login.php
+++ Software_Code/_site/manage_login.php
@@ -50,7 +50,7 @@ if (isset($_GET['edit'])) {
 					</li>
 				</ul>
 				<div class="d-flex">
-					<a class="btn btn-outline-light" href="login.php">Log Out</a>
+					<a class="btn btn-outline-light" href="assets/php/destroy_session.php">Log Out</a>
 				</div>
 			</div>
 		</div>
