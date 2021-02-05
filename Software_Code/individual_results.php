00a89ed2b27beae19e26ccb5601449ad9cda2a2d
MikolajOlejnik <molejnik@dundee.ac.uk>
2021-02-05 11:16:48 +0000

    Bug fix

--- Software_Code/individual_results.php
+++ Software_Code/individual_results.php
@@ -48,5 +48,5 @@ foreach ($_POST as $key => $value) {
 
 <!-- Back button, posts required questionnaire value. -->
 <form action="pr_show_results.php" method="POST">
-  <button name="questionnaire" type="submit" value="<?php echo $_SESSION['currQnaire'] ?>">Back</button>
+<?php echo "<button name='questionnaire' type='submit' value=" .$_SESSION['currQnaire']. ">Back</button> ";?>
 </form>
